771b4b54a4f33347c44a75beaf5e97016e4c2979
oblerion <82583559+oblerion@users.noreply.github.com>
2022-10-31 16:01:37 +0100

    Update love2d.js

--- js/love2d.js
+++ js/love2d.js
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-export {Love,Canvas,Color,disableRightClickMenu,collide,
+export {Love,Canvas,Color,disableRightClickMenu, disableConsole,collide,
 		math_random};
 const MOUSE = true;
 const KEYBOARD = false;

b295a3a907a119ed8d2df9471034e278ddc88712
Magnus Oblerion <82583559+oblerion@users.noreply.github.com>
2024-11-17 15:26:42 +0100

    Add files via upload

--- js/love2d.js
+++ js/love2d.js
@@ -97,10 +97,6 @@ class Love
         this.lastUpdate = 0;
         this.loading = 0;
     }
-    getTextWidth(text)
-    {
-    	return this.context.measureText(text);
-    }
 	system_writeSave(name,val)
 	{
 		if(typeof(name)=="string" &&
@@ -786,5 +782,5 @@ if (LOVE2D_TOUCH == true) {
 	_disableRightClickMenu();
 }
 window.requestAnimationFrame(_main_loop);
-//export {love,math_random};
+export {love,math_random};
 
